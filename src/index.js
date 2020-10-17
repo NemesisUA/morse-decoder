ca3a5b96c0370a2ba8919b4f857bedfdda18c7be
NemesisUA <display.n0ne.01@gmail.com>
2020-10-17 14:25:44 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let decoded = '';
+    for (let i = 0; i < expr.length ; i += 10) {
+        let symbol = '';
+        symbol = expr.substr(i, i + 10);
+        
+        // symbol to morse notation
+        let morseNotation = '';
+        for (j = 0; j < 10; j +=2) {
+            if (symbol[j] === '*')  morseNotation = ' ';
+            if (symbol[j] === '1') {
+                if (symbol[j + 1] === '0' ) {morseNotation = morseNotation + '.';
+                } else morseNotation = morseNotation + '-' ;   
+            }
+            
+        }  
+
+        // decode symbol in morse notation
+        if (morseNotation === ' ') {
+            decoded = decoded + ' ';
+        } else {
+            decoded = decoded + MORSE_TABLE[morseNotation];
+        }    
+
+
+    } return decoded;
 }
 
 module.exports = {
